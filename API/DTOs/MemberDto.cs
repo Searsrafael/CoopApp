067d2ac0077fa10b5130feb066f6a0c644070083
Rafaelsears <rafael.sears@dialexa.com>
2022-03-20 20:35:55 -0500

    End of section 8

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+         public int Id { get; set; } // Our Primary key, automatically updates when a new record is added
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+        public string Country { get; set; } 
+
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
